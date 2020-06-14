73e3a1cfdae147e924c499ee374e2be28d1704fb
Victoria Cheung <victoriakcheung@gmail.com>
2020-06-14 15:41:22 +0000

    update height-weight selection

--- src/data/query_inputs_to_mimic.py
+++ src/data/query_inputs_to_mimic.py
@@ -178,41 +178,24 @@ def build_query_adm() -> str:
 
 def build_query_htwt() -> str:
     sql = """
-    WITH chart_inputs AS
-        (SELECT
+    WITH height AS
+        (
+        SELECT
             chart.subject_id,
-            chart.hadm_id,
+            chart.itemid,
             chart.charttime,
-            chart.value,
+            chart.value AS height,
             chart.valueuom,
-            chart.warning,
-            inputs.starttime,
-            inputs.endtime,
-            inputs.itemid,
-            inputs.patientweight
+            height_label.label
         FROM
             mimiciii.chartevents as chart
-        LEFT JOIN
-            (SELECT
-                ie.subject_id,
-                ie.starttime,
-                ie.endtime,
-                ie.itemid,
-                ie.patientweight
-            FROM
-                mimiciii.inputevents_mv as ie
-            ) AS inputs
-            ON inputs.subject_id = chart.subject_id
-        ORDER BY chart.subject_id)
-        , hw AS
-        (
+        INNER JOIN
+            (
             SELECT
                 ditems.itemid,
                 ditems.label,
                 ditems.category,
-                ditems.unitname,
-                ditems.param_type,
-                ditems.conceptid
+                ditems.unitname
             FROM
                 mimiciii.d_items as ditems
             WHERE
@@ -223,37 +206,40 @@ def build_query_htwt() -> str:
                 ditems.itemid,
                 ditems.label,
                 ditems.category,
-                ditems.unitname,
-                ditems.param_type,
-                ditems.conceptid
+                ditems.unitname
             FROM
                 mimiciii.d_items as ditems
             WHERE
                 (lower(ditems.label) LIKE '%bed%') OR
                 (lower(ditems.label) LIKE '%apache%')
+            ) AS height_label
+            ON height_label.itemid = chart.itemid
         )
+
+    , weight AS
+        (
         SELECT
-            chart_inputs.subject_id,
-            chart_inputs.hadm_id,
-            chart_inputs.charttime,
-            chart_inputs.value,
-            chart_inputs.valueuom,
-            chart_inputs.warning,
-            chart_inputs.hadm_id,
-            chart_inputs.starttime,
-            chart_inputs.endtime,
-            chart_inputs.itemid,
-            hw.itemid,
-            hw.label,
-            hw.category,
-            hw.unitname,
-            hw.param_type,
-            hw.conceptid,
-            chart_inputs.patientweight
-        FROM chart_inputs
-        INNER JOIN hw
-            ON hw.itemid = chart_inputs.itemid
-        ORDER BY
-            chart_inputs.subject_id
+            ie.subject_id,
+            ie.starttime,
+            ie.endtime,
+            ie.itemid,
+            ie.patientweight
+        FROM
+            mimiciii.inputevents_mv as ie
+        )
+    SELECT
+        weight.subject_id,
+        weight.starttime,
+        weight.endtime,
+        weight.patientweight,
+        height.charttime,
+        height.label,
+        height.height,
+        height.valueuom as height_units
+    FROM weight
+    LEFT JOIN height
+        ON height.subject_id = weight.subject_id
+    ORDER BY
+        weight.subject_id
     """
     return dedent(sql)

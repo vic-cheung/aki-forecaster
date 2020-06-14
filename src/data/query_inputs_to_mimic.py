19c9749a754fa272aabfd48967bbe190eeba8a98
Victoria Cheung <victoriakcheung@gmail.com>
2020-06-14 17:23:48 +0000

    change height & weight queries to their own fns

--- src/data/query_inputs_to_mimic.py
+++ src/data/query_inputs_to_mimic.py
@@ -176,70 +176,57 @@ def build_query_adm() -> str:
     return dedent(sql)
 
 
-def build_query_htwt() -> str:
+def build_query_height() -> str:
     sql = """
-    WITH height AS
-        (
-        SELECT
-            chart.subject_id,
-            chart.itemid,
-            chart.charttime,
-            chart.value AS height,
-            chart.valueuom,
-            height_label.label
-        FROM
-            mimiciii.chartevents as chart
-        INNER JOIN
-            (
-            SELECT
-                ditems.itemid,
-                ditems.label,
-                ditems.category,
-                ditems.unitname
-            FROM
-                mimiciii.d_items as ditems
-            WHERE
-                (lower(ditems.label) LIKE '%admit ht') OR
-                (lower(ditems.label) LIKE '%height%')
-            EXCEPT
-            SELECT
-                ditems.itemid,
-                ditems.label,
-                ditems.category,
-                ditems.unitname
-            FROM
-                mimiciii.d_items as ditems
-            WHERE
-                (lower(ditems.label) LIKE '%bed%') OR
-                (lower(ditems.label) LIKE '%apache%')
-            ) AS height_label
-            ON height_label.itemid = chart.itemid
-        )
-
-    , weight AS
-        (
-        SELECT
-            ie.subject_id,
-            ie.starttime,
-            ie.endtime,
-            ie.itemid,
-            ie.patientweight
-        FROM
-            mimiciii.inputevents_mv as ie
-        )
     SELECT
-        weight.subject_id,
-        weight.starttime,
-        weight.endtime,
-        weight.patientweight,
-        height.charttime,
-        height.label,
-        height.height,
-        height.valueuom as height_units
-    FROM weight
-    LEFT JOIN height
-        ON height.subject_id = weight.subject_id
+        chart.subject_id,
+        chart.charttime,
+        chart.value AS height,
+        chart.valueuom
+    FROM
+        mimiciii.chartevents as chart
+    INNER JOIN
+        (
+        SELECT
+            ditems.itemid,
+            ditems.label,
+            ditems.category,
+            ditems.unitname
+        FROM
+            mimiciii.d_items as ditems
+        WHERE
+            (lower(ditems.label) LIKE '%admit ht') OR
+            (lower(ditems.label) LIKE '%height%')
+        EXCEPT
+        SELECT
+            ditems.itemid,
+            ditems.label,
+            ditems.category,
+            ditems.unitname
+        FROM
+            mimiciii.d_items as ditems
+        WHERE
+            (lower(ditems.label) LIKE '%bed%') OR
+            (lower(ditems.label) LIKE '%apache%')
+        ) AS height_label
+        ON height_label.itemid = chart.itemid
     ORDER BY
-        weight.subject_id
+        chart.subject_id
+    """
+    return dedent(sql)
+
+
+def build_query_weight() -> str:
+    sql = """
+    SELECT
+        ie.subject_id,
+        ie.starttime,
+        ie.endtime,
+        ie.itemid,
+        ie.patientweight
+    FROM
+        mimiciii.inputevents_mv as ie
+    ORDER BY
+        ie.subject_id
     """
     return dedent(sql)

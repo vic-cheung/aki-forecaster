d5a78b3f38df62a242b5ff8e40ede86a5025ad30
Victoria Cheung <victoriakcheung@gmail.com>
2020-06-16 14:58:32 +0000

    process only continuous variables

--- src/features/featurizations.py
+++ src/features/featurizations.py
@@ -1,39 +1,38 @@
 # %%
 import pandas as pd
-import numpy as np
+
+# import numpy as np
 from pathlib import Path
 
 pd.set_option("display.max_columns", None)
 pd.set_option("display.max_rows", None)
-
-
 
 #%%
 subj_id_labs = Path("/home/victoria/aki-forecaster/data/interim/subj_id_labs")
 csv_files = [file for file in subj_id_labs.iterdir()]
 csv_file = csv_files[1]
 pt_data = pd.read_csv(csv_file, header=0, index_col=0)
-
+res = pt_data.loc[:, ~pt_data.columns.str.contains(pat="_cat")]
 #%%
-pt_data = pt_data.fillna("None")
+# pt_data = pt_data.fillna("None")
 #%%
-series = pt_data["Nitrite[U-H]"]
+# series = pt_data["Nitrite[U-H]"]
 
 
-def categorical_to_one_hot(series: pd.Series) -> pd.DataFrame:
-    "Transforms all categorical columns into multiple one-hot columns."
-    categories = series.unique()
-    encoder = OneHotEncoder()
-    # Generate OneHotEncoder
-    encoder.fit(categories.reshape(-1, 1))
-    # Transform all values in series to One-Hot Encoding
-    one_hot = encoder.transform(series.values.reshape(-1, 1)).toarray()
-    # Return pd.DataFrame that is original Series in One-Hot Encoding
-    one_hot_column_names = [series.name + ">" + cat for cat in encoder.categories_]
-    return pd.DataFrame(one_hot, columns=one_hot_column_names).as
+# def categorical_to_one_hot(series: pd.Series) -> pd.DataFrame:
+#     "Transforms all categorical columns into multiple one-hot columns."
+#     categories = series.unique()
+#     encoder = OneHotEncoder()
+#     # Generate OneHotEncoder
+#     encoder.fit(categories.reshape(-1, 1))
+#     # Transform all values in series to One-Hot Encoding
+#     one_hot = encoder.transform(series.values.reshape(-1, 1)).toarray()
+#     # Return pd.DataFrame that is original Series in One-Hot Encoding
+#     one_hot_column_names = [series.name + ">" + cat for cat in encoder.categories_]
+#     return pd.DataFrame(one_hot, columns=one_hot_column_names).as
 
 
-res = categorical_to_one_hot(series)
+# res = categorical_to_one_hot(series)
 
 #%%
 
@@ -63,10 +62,9 @@ def create_samples(data: pd.DataFrame, window: int = 3):
     # ]
     # continuous = list(np.setdiff1d(data.columns, categorical))
     # # Interpolate NaN values of numerical features
-    # data.loc[:, continuous] = (
-    #     data.loc[:, continuous]
-    #     .apply(pd.to_numeric, errors="coerce")
-    #     .interpolate(limit_direction="both")
+    data = data.apply(pd.to_numeric, errors="coerce").interpolate(
+        limit_direction="both"
+    )
     # )
     # #
     # try:
@@ -75,8 +73,8 @@ def create_samples(data: pd.DataFrame, window: int = 3):
     # except Exception:
     #     print("No values in cat_mode")
 
-    X_data_raw = []
-    Y_data_raw = []
+    X_raw = []
+    Y_raw = []
 
     # get midnight (start of first day)
     last_date = data.index[-1]
@@ -98,36 +96,29 @@ def create_samples(data: pd.DataFrame, window: int = 3):
         if (not x.empty) and (not y.empty):
             if not y["Creatinine[B-C]"].empty:
                 # Add X to Dataset
-                X_data_raw += [x]
+                X_raw += [x]
                 # Add Y to Dataset
-                Y_data_raw += [y]
+                Y_raw += [y]
 
         # Move Start Interval
         start_date = start_date + pd.Timedelta(days=1)
-    return X_data_raw, Y_data_raw
+    return X_raw, Y_raw
 
 
-def featurize_X(X_data_raw: pd.DataFrame, categorical: list, continuous: list):
+def featurize_X(X_raw: pd.DataFrame):
     "Returns pd.DataFrame of Labs"
     X = []
     means_raw = []
-    modes_raw = []
-    for x in X_data_raw:
-        means = x.loc[:, continuous].mean()
-        # means.index = [item + "_avg" for item in means.index]
-        modes = x.loc[:, categorical].iloc[-1]
-        # modes.index = [item + "_mode" for item in modes.index]
+    for x in X_raw:
+        means = x.mean()
         means_raw += [means]
-        modes_raw += [modes]
-    mean_df = pd.DataFrame(means_raw)
-    mode_df = pd.DataFrame(modes_raw).reset_index(drop=True)
-    X = pd.concat([mean_df, mode_df], axis=1)
+    X = pd.DataFrame(means_raw)
     return X
 
 
-def featurize_Y(Y_data_raw: pd.DataFrame):
+def featurize_Y(Y_raw: pd.DataFrame):
     "Returns pd.Series of Creatinine for day after X"
-    Y_cr_only = [Y["Creatinine[B-C]"] for Y in Y_data_raw]
+    Y_cr_only = [Y["Creatinine[B-C]"] for Y in Y_raw]
     Y = pd.Series([item.mean() for item in Y_cr_only], name="Creatinine_avg")
     return Y
 
@@ -137,14 +128,15 @@ def featurize(csv_file):
     Splits raw lab data into samples based on sliding window in `create_samples`.
     Featurize `X`, then Featurize `Y.  Returns `X` & `Y`.
     """
-    pt_data = pd.read_csv(csv_file, header=[0, 1], index_col=0)
+    pt_data = pd.read_csv(csv_file, header=0, index_col=0)
+    pt_data = pt_data.loc[:, ~pt_data.columns.str.contains(pat="_cat")]
 
     # Only create training example if patient has Creatinine Value (corresponding to itemid: 50912)
     if "Creatinine[B-C]" in pt_data.columns:
         # Try to Preprocess Data
         try:
-            X_raw, Y_raw, categorical, continuous = create_samples(pt_data)
-            x = featurize_X(X_raw, categorical, continuous)
+            X_raw, Y_raw = create_samples(pt_data)
+            x = featurize_X(X_raw)
             y = featurize_Y(Y_raw)
             return x, y
             if x.empty or y.empty:

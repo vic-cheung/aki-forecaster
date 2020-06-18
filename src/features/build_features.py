3b55168528530d9418449573d0ce904f39eecf6d
Victoria Cheung <victoriakcheung@gmail.com>
2020-06-18 06:17:38 +0000

    updated build_features.py for hadm_id data format

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -1,4 +1,3 @@
-#%%
 import pandas as pd
 from pathlib import Path
 from src.features.featurizations import featurize
@@ -9,18 +8,14 @@ pd.set_option("display.max_columns", None)
 pd.set_option("display.max_rows", None)
 
 
-#%%
-# Paths to Each Patient's Lab Data
+#%% Paths to Each Patient's Lab Data
 hadm_id_labs = Path("/home/victoria/aki-forecaster/data/interim/hadm_id_labs")
 csv_files = [file for file in hadm_id_labs.iterdir()]
 
-#%%
-# Make all labels unique by simplfying & then adding fluid & category info
-data_dir = Path("/home/victoria/aki-forecaster/data/raw_hadmid")
+#%% Make all labels unique by simplfying & then adding fluid & category info
+data_dir = Path("/home/victoria/aki-forecaster/data/raw")
 
-# %%
-
-# Use Concurrent to featurize samples
+# %% Use Concurrent to featurize samples
 executor = ProcessPoolExecutor()
 jobs = [executor.submit(featurize, csv_file) for csv_file in csv_files]
 
@@ -40,13 +35,9 @@ Y = [item for item in Y if not item.empty]
 X = pd.concat(X, axis=0)
 Y = pd.concat(Y, axis=0)
 
-# %%
-# Save Train Data
+# %% Save Train Data
 processed_dir = Path(
     "/home/victoria/aki-forecaster/data/processed/continuous_vars_only/hadm_id"
 )
 X.to_csv(processed_dir / "X.csv")
 Y.to_csv(processed_dir / "Y.csv")
-
-
-# %%

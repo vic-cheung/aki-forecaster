1a9a62cc611a6f108f1c6cd0840d32104165f790
Victoria Cheung <victoriakcheung@gmail.com>
2020-06-18 02:25:53 +0000

    fix lab values query to now include alkaline phosphatase & aspartate aminotransferase correctly

--- src/data/query_inputs_to_mimic.py
+++ src/data/query_inputs_to_mimic.py
@@ -2,6 +2,131 @@ from textwrap import dedent
 
 
 def build_query_lab_events(
+    blood_labels_list: list = None, urine_labels_list: list = None, limit: int = None
+) -> str:
+    blood_labels = (
+        blood_labels_list
+        if blood_labels_list
+        else [
+            "potassium",
+            "sodium",
+            "chloride",
+            "bicarbonate",
+            "creatinine",
+            "glucose",
+            "phosphate",
+            "a1c",
+            "white blood cells",
+            "wbc",
+            "rbc",
+            "red blood cells",
+            "hemoglobin",
+            "hematocrit",
+            "platelet",
+            "mcv",
+            "aminotransferase",
+            "(alt)",
+            "(ast)",
+            "bilirubin",
+            "alkaline phosphatase",
+            "troponin",
+            "bnp",
+            "c-reactive protein",
+            "sedimentation",
+            "neutrophil",
+        ]
+    )
+    blood_str = " OR\n".join([f"lower(li.label) LIKE '%{x}%'" for x in blood_labels])
+    urine_labels = (
+        urine_labels_list
+        if urine_labels_list
+        else [
+            "yeast",
+            "urine appearance",
+            "urine creatinine",
+            "ketone",
+            "urine volume",
+            "osmolality, urine",
+            "bacteria",
+            "protein/creatinine ratio",
+            "nitrite",
+            "rbc",
+            "cellular cast",
+            "wbc casts",
+            "leukocytes",
+            "albumin, urine",
+            "specific gravity",
+            "rbc casts",
+            "ph",
+            "protein",
+            "hyaline casts",
+            "albumin/creatinine, urine",
+            "broad casts",
+            "urine casts, other",
+            "wbc",
+            "blood",
+            "granular casts",
+            "urine volume, total",
+            "urine color",
+            "sodium, urine",
+        ]
+    )
+    urine_str = " OR\n".join([f"lower(li.label) LIKE '%{x}%'" for x in urine_labels])
+    urine_labels_except = [
+        "wbc clumps",
+        "rbc clumps",
+        "24 hr",
+        "porphobilinogen screen",
+        "crystals",
+        "prot. electrophoresis",
+        "eosinophils",
+        "amorphous",
+        "amphetamine",
+        "hyphenated",
+        "total protein",
+        "phosphate",
+    ]
+    urine_except_str = " AND\n".join(
+        [f"lower(li.label) NOT LIKE '%{x}%'" for x in urine_labels_except]
+    )
+    sql = f"""
+        SELECT
+            le.subject_id,
+            le.hadm_id,
+            le.itemid,
+            le.charttime,
+            le.value,
+            le.valueuom,
+            -- le.flag
+            li.label,
+            li.fluid,
+            li.category,
+            li.loinc_code
+        FROM mimiciii.labevents as le
+        INNER JOIN mimiciii.d_labitems AS li
+            ON li.itemid = le.itemid
+        WHERE
+            (
+                lower(li.fluid) = 'blood' AND ({blood_str})
+            )
+            OR
+            (
+                lower(li.fluid) = 'urine' AND 
+                (
+                    ({urine_str}) AND ({urine_except_str})
+                )
+            )
+        ORDER BY
+            le.subject_id ASC,
+            le.hadm_id ASC,
+            le.charttime ASC
+    """
+    if limit:
+        sql += f"\nLIMIT {limit}"
+    return dedent(sql)
+
+
+def build_query_lab_items_id(
     blood_lab_labels_list: list = None, urine_lab_labels_list: list = None
 ) -> str:
     blood_lab_labels = (
@@ -82,63 +207,6 @@ def build_query_lab_events(
 
     sql = f"""
     SELECT
-        le.hadm_id,
-        le.itemid AS labevents_itemid,
-        le.charttime,
-        labitems.label,
-        labitems.fluid,
-        labitems.category,
-        labitems.loinc_code,
-        le.value AS labevents_value,
-        le.valueuom,
-        le.flag
-    FROM mimiciii.labevents as le
-    INNER JOIN
-        (SELECT
-            li.itemid,
-            li.label,
-            li.fluid,
-            li.category,
-            li.loinc_code
-        FROM
-            mimiciii.d_labitems AS li
-        WHERE
-            {blood_lab_labels_where} OR
-            {urine_lab_labels_where}
-        EXCEPT
-        SELECT
-            li.itemid,
-            li.label,
-            li.fluid,
-            li.category,
-            li.loinc_code
-        FROM
-            mimiciii.d_labitems AS li
-        WHERE
-            (lower(li.fluid) = 'urine' AND lower(li.label) LIKE '%wbc clumps%') OR
-            (lower(li.fluid) = 'urine' AND lower(li.label) LIKE '%rbc clumps%') OR
-            (lower(li.fluid) = 'urine' AND lower(li.label) LIKE '%24 hr%') OR
-            (lower(li.fluid) = 'urine' AND lower(li.label) LIKE '%porphobilinogen screen%')  OR
-            (lower(li.fluid) = 'urine' AND lower(li.label) LIKE '%crystals%')  OR
-            (lower(li.fluid) = 'urine' AND lower(li.label) LIKE '%prot. electrophoresis%')  OR
-            (lower(li.fluid) = 'urine' AND lower(li.label) LIKE '%eosinophils%')  OR
-            (lower(li.fluid) = 'urine' AND lower(li.label) LIKE '%amorphous%')  OR
-            (lower(li.fluid) = 'urine' AND lower(li.label) LIKE '%amphetamine%')  OR
-            (lower(li.fluid) = 'urine' AND lower(li.label) LIKE '%hyphenated%')  OR
-            (lower(li.fluid) = 'urine' AND lower(li.label) LIKE '%total protein%')  OR
-            (lower(li.fluid) = 'urine' AND lower(li.label) LIKE '%phosphate%')
-        ) AS labitems
-    ON labitems.itemid = le.itemid
-    ORDER BY
-        le.hadm_id,
-        le.charttime
-    """
-    return dedent(sql)
-
-
-def build_query_lab_items_id() -> str:
-    sql = """
-    SELECT
         li.itemid,
         li.label,
         li.fluid,
@@ -147,60 +215,10 @@ def build_query_lab_items_id() -> str:
     FROM
         mimiciii.d_labitems AS li
     WHERE
-        (lower(li.fluid) = 'blood' AND lower(li.label) LIKE '%potassium%') OR
-        (lower(li.fluid) = 'blood' AND lower(li.label) LIKE '%sodium%') OR
-        (lower(li.fluid) = 'blood' AND lower(li.label) LIKE '%chloride%') OR
-        (lower(li.fluid) = 'blood' AND lower(li.label) LIKE '%bicarbonate%') OR
-        (lower(li.fluid) = 'blood' AND lower(li.label) LIKE '%creatinine%') OR
-        (lower(li.fluid) = 'blood' AND lower(li.label) LIKE '%glucose%') OR
-        (lower(li.fluid) = 'blood' AND lower(li.label) LIKE '%phosphate%') OR
-        (lower(li.fluid) = 'blood' AND lower(li.label) LIKE '%a1c%') OR
-        (lower(li.fluid) = 'blood' AND lower(li.label) LIKE '%wbc%') OR
-        (lower(li.fluid) = 'blood' AND lower(li.label) LIKE '%white blood cells%') OR
-        (lower(li.fluid) = 'blood' AND lower(li.label) LIKE '%red blood cells%') OR
-        (lower(li.fluid) = 'blood' AND lower(li.label) LIKE 'hemoglobin') OR
-        (lower(li.fluid) = 'blood' AND lower(li.label) LIKE '%hematocrit%') OR
-        (lower(li.fluid) = 'blood' AND lower(li.label) LIKE '%platelet%') OR
-        (lower(li.fluid) = 'blood' AND lower(li.label) LIKE '%mcv%') OR
-        (lower(li.fluid) = 'blood' AND lower(li.label) LIKE '%alanine aminotransferase%') OR
-        (lower(li.fluid) = 'blood' AND lower(li.label) LIKE '%asparate aminotransferase%') OR
-        (lower(li.fluid) = 'blood' AND lower(li.label) LIKE '%bilirubin%') OR
-        (lower(li.fluid) = 'blood' AND lower(li.label) LIKE 'alkaline phosphatase')OR
-        (lower(li.fluid) = 'blood' AND lower(li.label) LIKE '%troponin%')OR
-        (lower(li.fluid) = 'blood' AND lower(li.label) LIKE '%bnp%') OR
-        (lower(li.fluid) = 'blood' AND lower(li.label) LIKE '%c-reactive protein%') OR
-        (lower(li.fluid) = 'blood' AND lower(li.label) LIKE '%sedimentation%') OR
-        (lower(li.fluid) = 'blood' AND lower(li.label) LIKE '%neutrophil%') OR
-        (lower(li.fluid) = 'urine' AND lower(li.label) LIKE '%bacteria%') OR
-        (lower(li.fluid) = 'urine' AND lower(li.label) LIKE '%blood%') OR
-        (lower(li.fluid) = 'urine' AND lower(li.label) LIKE '%broad casts%') OR
-        (lower(li.fluid) = 'urine' AND lower(li.label) LIKE '%cellular cast%') OR
-        (lower(li.fluid) = 'urine' AND lower(li.label) LIKE '%albumin, urine%') OR
-        (lower(li.fluid) = 'urine' AND lower(li.label) LIKE '%albumin/creatinine, urine%') OR
-        (lower(li.fluid) = 'urine'AND lower(li.label) LIKE '%osmolality, urine%') OR
-        (lower(li.fluid) = 'urine' AND lower(li.label) LIKE '%ph%') OR
-        (lower(li.fluid) = 'urine' AND lower(li.label) LIKE '%protein/creatinine ratio%') OR
-        (lower(li.fluid) = 'urine' AND lower(li.label) LIKE '%sodium, urine%') OR
-        (lower(li.fluid) = 'urine' AND lower(li.label) LIKE '%urine creatinine%') OR
-        (lower(li.fluid) = 'urine' AND lower(li.label) LIKE '%urine volume%') OR
-        (lower(li.fluid) = 'urine' AND lower(li.label) LIKE '%urine volume, total%') OR
-        (lower(li.fluid) = 'urine' AND lower(li.label) LIKE '%granular casts%') OR
-        (lower(li.fluid) = 'urine' AND lower(li.label) LIKE '%hyaline casts%') OR
-        (lower(li.fluid) = 'urine' AND lower(li.label) LIKE '%ketone%') OR
-        (lower(li.fluid) = 'urine' AND lower(li.label) LIKE '%leukocytes%') OR
-        (lower(li.fluid) = 'urine' AND lower(li.label) LIKE '%nitrite%') OR
-        (lower(li.fluid) = 'urine' AND lower(li.label) LIKE '%protein%') OR
-        (lower(li.fluid) = 'urine' AND lower(li.label) LIKE '%rbc%') OR
-        (lower(li.fluid) = 'urine' AND lower(li.label) LIKE '%rbc casts%') OR
-        (lower(li.fluid) = 'urine' AND lower(li.label) LIKE '%specific gravity%') OR
-        (lower(li.fluid) = 'urine' AND lower(li.label) LIKE '%urine appearance%') OR
-        (lower(li.fluid) = 'urine' AND lower(li.label) LIKE '%urine casts, other%') OR
-        (lower(li.fluid) = 'urine' AND lower(li.label) LIKE '%urine color%') OR
-        (lower(li.fluid) = 'urine' AND lower(li.label) LIKE '%wbc%') OR
-        (lower(li.fluid) = 'urine' AND lower(li.label) LIKE '%wbc casts%') OR
-        (lower(li.fluid) = 'urine' AND lower(li.label) LIKE '%yeast%')
+        {blood_lab_labels_where} OR
+        {urine_lab_labels_where}
     EXCEPT
-        SELECT
+    SELECT
         li.itemid,
         li.label,
         li.fluid,
@@ -238,7 +256,7 @@ def build_query_kidney_events() -> str:
         WHERE
             (lower(icd.short_title) LIKE '%kidney%') OR
             (lower(icd.short_title) LIKE 'renal%')
-        ORDER BY 
+        ORDER BY
             diag.hadm_id
         """
     return dedent(sql)
